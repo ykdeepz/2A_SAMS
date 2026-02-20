79d963288718fd16e76cbdc71b94dc16b5f123e8
ykdeepz <paryani.sandeep28@gmail.com>
2026-02-20 15:44:32 +0000

    laswan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,53 @@
 import { Routes } from '@angular/router';
+import { Logout } from './components/auth/logout/logout';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'login', pathMatch: 'full' },
-	{
-		path: 'login',
-		loadComponent: () => import('./components/auth/login/login').then(m => m.LoginComponent)
-	},
-	{
-		path: 'signup',
-		loadComponent: () => import('./components/auth/login/login').then(m => m.LoginComponent),
-		data: { signUp: true }
-	},
-	{
-		path: 'dashboard',
-		children: [
-			{ path: '', redirectTo: 'instructor', pathMatch: 'full' },
-			{ path: 'admin', loadComponent: () => import('./components/dashboard/admin/admin').then(m => m.Admin) },
-			{ path: 'instructor', loadComponent: () => import('./components/dashboard/instructor/instructor').then(m => m.Instructor) },
-			{ path: 'parent', loadComponent: () => import('./components/dashboard/parent/parent').then(m => m.Parent) },
-			{ path: 'student', loadComponent: () => import('./components/dashboard/student/student').then(m => m.Student) }
-		]
-	}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/auth/login/login').then(m => m.LoginComponent)
+  },
+
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./components/auth/login/login').then(m => m.LoginComponent),
+    data: { signUp: true }
+  },
+
+  {
+  path: 'logout',
+  loadComponent: () =>
+    import('./components/auth/login/login')
+      .then(m => m.LoginComponent)
+},
+
+  {
+    path: 'dashboard',
+    children: [
+      { path: '', redirectTo: 'instructor', pathMatch: 'full' },
+      {
+        path: 'admin',
+        loadComponent: () =>
+          import('./components/dashboard/admin/admin').then(m => m.Admin)
+      },
+      {
+        path: 'instructor',
+        loadComponent: () =>
+          import('./components/dashboard/instructor/instructor').then(m => m.Instructor)
+      },
+      {
+        path: 'parent',
+        loadComponent: () =>
+          import('./components/dashboard/parent/parent').then(m => m.Parent)
+      },
+      {
+        path: 'student',
+        loadComponent: () =>
+          import('./components/dashboard/student/student').then(m => m.Student)
+      }
+    ]
+  }
 ];
